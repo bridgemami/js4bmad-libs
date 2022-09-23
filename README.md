c786d66b29dfbc8787086225f5dcf9406e5eb0d5
bridgemami <mdbridgeman@gmail.com>
2022-09-23 16:25:17 -0700

    cut the space

---
+++ README.md
@@ -0,0 +1,4 @@
+# JS4B - Mad Libs
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rockygg/pen/eYZxeaN](https://codepen.io/rockygg/pen/eYZxeaN).
+
---
+++ script.js
@@ -0,0 +1,53 @@
+// Assign STRING VALUES ONLY to all of the following variables.
+// Assign an adjective
+let adj1 ='big';
+// Assign an adjective
+let adj2 = 'little';
+// Assign an adjective
+let adj3 = 'ugly';
+// Assign an adjective
+let adj4 = 'cold';
+// Assign an adjective
+let adj5 = 'warm';
+// Assign an adjective
+let adj6 = 'thick';
+// Assign an adverb
+let adverb = 'quickly';
+// Assign a color
+let color = 'green';
+// Assign a noun
+let noun1 = 'plant';
+// Assign a noun
+let noun2 = 'salad';
+// Assign a noun
+let noun3 = 'sheep';
+// Assign a noun
+let noun4 = 'moth';
+// Assign a noun
+let noun5 = 'stew';
+// Assign a noun
+let noun6 = 'germs';
+// Assign a number
+let num = 9;
+// Assign a plural noun
+let pnoun1 = 'plants';
+
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
+
+
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog.`;
+
+//Console log the completed story
+console.log(story);
